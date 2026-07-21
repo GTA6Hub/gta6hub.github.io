7cc7011fb21aecaf26d14280b9a64411bd806d9b
gta6hub <gtaivnj@gmail.com>
2026-07-21 03:01:37 +0100

    fix: base path para repositorio de utilizador

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gta6hub.github.io/',
+  base: '',
 })
